946b07f4f7796bd2fb7235771d8a0b6a2e4d23f8
Ben Ohayon <ben1679@gmail.com>
2026-04-16 22:46:45 +0300

    implemented PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          { src: 'assets/logo.svg', sizes: '192x192', type: 'image/svg+xml' },
+          { src: 'assets/logo.svg', sizes: '512x512', type: 'image/svg+xml' }
+        ]
+      }
+    })
   ],
 })
